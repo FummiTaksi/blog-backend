85bf608b6eea835208be2ed99b17444bd729b021
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 11:51:58 +0200

    changed port and establishing mongoDB connection#

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const url = "mongodb://user:password@ds247047.mlab.com:47047/blog-development"
+mongoose.connect(url, {useMongoClient: true})
+mongoose.Promise = global.Promise;
+
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -11,6 +17,6 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
-const port = 3001
+const port = 3003
 app.listen(port)
 console.log(`Server running on port ${port}`)
