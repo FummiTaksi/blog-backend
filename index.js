520ec820515cd97f166a1cce124c25f28eb267bb
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 12:12:27 +0200

    post works

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const Blog = mongoose.model('Blog', {
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+  
+  module.exports = Blog
+
 const url = "mongodb://user:password@ds247047.mlab.com:47047/blog-development"
 mongoose.connect(url, {useMongoClient: true})
 mongoose.Promise = global.Promise;
@@ -16,6 +25,17 @@ app.use(cors())
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
+
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+})
 
 const port = 3003
 app.listen(port)
