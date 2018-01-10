bccd7f56aca2a895ed9bac1e073ecea8b23eac76
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 14:12:35 +0200

    tested totalLikes method

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,7 +1,34 @@
-const list = require('../utils/list_helper')
+const listHelper = require('../utils/list_helper')
 
 test('dummy is called', () => {
   const blogs = []
-  const result = list.dummy(blogs)
+  const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const emptyList = []
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('result is 0 with empty list', () => {
+    
+    const result = listHelper.totalLikes(emptyList)
+    expect(result).toBe(0)
+  })
 })
