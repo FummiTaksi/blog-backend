00264daa37632c52c01407a640863af35cfb6694
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 14:12:24 +0200

    method totalLikes implemented

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+      return blogs.reduce((previous, next) => {
+          console.log("previous",previous)
+          console.log("next",next)
+          return previous + next.likes
+      },0)
+  }
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
