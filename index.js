3600fb2ed2bac6d0d7252d53675a5dbc05126583
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 11:46:11 +0200

    installed cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.json())
-
+app.use(cors())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
