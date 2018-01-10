5b60bef538a5ba8b295f172771f1ebf7e449ac13
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 12:33:34 +0200

    using person model

--- index.js
+++ index.js
@@ -3,21 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  module.exports = Blog
-
-const url = "mongodb://user:password@ds247047.mlab.com:47047/blog-development"
-mongoose.connect(url, {useMongoClient: true})
-mongoose.Promise = global.Promise;
-
+const Blog = require('./models/blog')
 
 app.use(bodyParser.json())
 app.use(cors())
