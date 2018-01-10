61e3bd924b79b7b5c66a5347f5bc30d750adb59b
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 21:15:15 +0200

    implemented delete method

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -27,4 +27,15 @@ blogsRouter.post('/', async(request, response) => {
     response.status(201).json(formatBlog(result))
 })
 
+blogsRouter.delete('/:id', async(request, response) => {
+    try {
+        const result = await Blog.findByIdAndRemove(request.params.id)
+        return response.status(204).json(result)
+    }
+    catch(error) {
+        return response.status(400).json(error)
+    }
+
+})
+
 module.exports =  blogsRouter
