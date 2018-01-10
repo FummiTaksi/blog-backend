7312142d9e570f652f90c16bbeb041878962cd57
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 15:49:35 +0200

    implemented mostBlogs method

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,6 +11,21 @@ const dummy = (blogs) => {
   const favoriteBlog = (blogs) => {
       return blogs.slice().sort((a,b) => {return a.likes < b.likes})[0];
   }  
+
+  const mostBlogs = (blogs) => {
+      
+      const startObject = {author: 'no authors in this list!', amount: 0}
+      return  blogs.reduce((previous,next) => {
+        const blogsWithThisAuthor = blogs.filter((blog) => {
+            return next.author === blog.author
+        })
+        const numberOfAuthors = blogsWithThisAuthor.length
+        if (numberOfAuthors > previous.amount) {
+            return {author: next.author, amount: numberOfAuthors}
+        }
+        return previous
+      },startObject)
+  }
   module.exports = {
-    dummy, totalLikes, favoriteBlog
+    dummy, totalLikes, favoriteBlog, mostBlogs
   }
