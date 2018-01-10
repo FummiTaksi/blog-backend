3c22b3b9dad93bcb4002dc69608fa228757049cb
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 16:39:26 +0200

    refactored mostBlogs, implemented mostLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,20 +12,35 @@ const dummy = (blogs) => {
       return blogs.slice().sort((a,b) => {return a.likes < b.likes})[0];
   }  
 
+  const blogsWithAuthor = (blogs, authorName) => {
+      return blogs.filter((blog) => {
+          return blog.author === authorName;
+      })
+  }
+
   const mostBlogs = (blogs) => {
       
       const startObject = {author: 'no authors in this list!', amount: 0}
       return  blogs.reduce((previous,next) => {
-        const blogsWithThisAuthor = blogs.filter((blog) => {
-            return next.author === blog.author
-        })
-        const numberOfAuthors = blogsWithThisAuthor.length
+        const numberOfAuthors = blogsWithAuthor(blogs,next.author).length
         if (numberOfAuthors > previous.amount) {
             return {author: next.author, amount: numberOfAuthors}
         }
         return previous
       },startObject)
   }
+
+  const mostLikes = (blogs) => {
+      const startObject = {author: 'no authors in this list!', votes: 0}
+      return blogs.reduce((previous, next) => {
+          const blogsByAuthor = blogsWithAuthor(blogs, next.author)
+          const votes = totalLikes(blogsByAuthor)
+          if (votes > previous.votes) {
+              return {author: next.author, votes: votes}
+          }
+          return previous
+      },startObject)
+  } 
   module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes, favoriteBlog, mostBlogs,mostLikes
   }
