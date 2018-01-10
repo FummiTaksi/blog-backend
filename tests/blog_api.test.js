7f6f1b830880952d8154f0e62f990dac5cd568c8
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 18:18:53 +0200

    written api tests for GET blogs

---
+++ tests/blog_api.test.js
@@ -0,0 +1,50 @@
+const supertest = require('supertest')
+const { app, server } = require('../index')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "Full-stack ohjelmointi",
+        author: "Matti Luukkainen",
+        url: "mluukkai.github.io",
+        likes: 250
+    },
+    {
+        title: "OHjelmoinnin perusteet",
+        author: "Arto Hellas",
+        url: "ohpe.fi",
+        likes: 90
+    }
+]
+beforeAll(async () => {
+    await Blog.remove({})
+  
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
+describe('GET /api/notes', () => {
+
+    test(' blogs are returned as json', async () => {
+        await api
+          .get('/api/blogs')
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+      })
+      
+      test(' all notes are returned', async () => {
+          const response = await api.get('/api/blogs')
+          expect(response.body.length).toBe(initialBlogs.length)
+      })
+
+})
+
+
+
+afterAll(() => {
+  server.close()
+})
