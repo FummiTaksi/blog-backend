5d183c2c16c8b3438e618982e079682e5adc50ce
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 17:41:24 +0200

    gets port from config file

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const Blog = require('./models/blog')
 const blogRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
 app.use('/api/blogs',blogRouter)
 
-const port = 3003
-app.listen(port)
+const port = config.port
+const mongoUrl = config.mongoUrl
+
+app.listen(config.port)
 console.log(`Server running on port ${port}`)
