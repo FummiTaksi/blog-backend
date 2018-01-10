aa7ea46c43eebca88fac8501a41758be43801606
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-10 12:50:07 +0200

    using controller

--- index.js
+++ index.js
@@ -4,28 +4,12 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
 app.use(bodyParser.json())
 app.use(cors())
-
-  app.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-})
+app.use(express.static('build'))
+app.use('/api/blogs',blogRouter)
 
 const port = 3003
 app.listen(port)
