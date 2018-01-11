66cf113e59a29e6e45bcff7b606377aa1c8eb7da
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-11 20:20:58 +0200

    removed connecting to db from here

--- models/blog.js
+++ models/blog.js
@@ -1,8 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-
-mongoose.connect(config.mongoUrl, {useMongoClient: true})
-mongoose.Promise = global.Promise;
 
 const Blog = mongoose.model('Blog', {
     title: String,
