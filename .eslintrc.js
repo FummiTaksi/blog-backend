b60816c5d8558f0d3ab1ebd53fc4452613eb28c9
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-14 20:06:59 +0200

    started using eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "globals": {
+        "process": true,
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-console": 0,
+        "no-process-env": 0
+    }
+};
